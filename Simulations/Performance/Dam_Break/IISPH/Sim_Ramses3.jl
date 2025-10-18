0f71e6f705f63d4a0bf8a681a6d6131f3f1acf0d
noahstruschka <noah.struschka@gmail.com>
2025-10-18 21:04:28 +0200

    .

--- Simulations/Performance/Dam_Break/IISPH/Sim_Ramses3.jl
+++ Simulations/Performance/Dam_Break/IISPH/Sim_Ramses3.jl
@@ -63,10 +63,10 @@ boundary_density_calculator = PressureZeroing()
 
 # IISPH parameters
 time_step = 0.0003
-omega = 0.6
+omega = 0.5
 min_iterations = 1
 max_iterations = 10
-max_error = 0.4
+max_error = 0.6
 
 
 
