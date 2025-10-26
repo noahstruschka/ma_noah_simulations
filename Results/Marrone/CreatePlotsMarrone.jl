0719e4415b542fedf70e7a43645dbb62d8980e27
noahstruschka <noah.struschka@gmail.com>
2025-10-26 12:42:53 +0100

    Created Plots

--- Results/Marrone/CreatePlotsMarrone.jl
+++ Results/Marrone/CreatePlotsMarrone.jl
@@ -14,7 +14,7 @@ save_files = true
 include("../../PlottingFiles/PlotDamBreak.jl")
 
 
-plot_dam_break_marrone("Output/Marrone/IISPH/PressureZeroing/IISPH_PressureZeroing_Marrone_fluid_1_7052.vtu",
+plot_dam_break_marrone("Output/Marrone/IISPH/PressureZeroing/IISPH_PressureZeroing_Marrone_fluid_1_7053.vtu",
                         "IISPH_Marrone_PressureZeroing2", save_fig=save_files)
 
 plot_dam_break_marrone("Output/Marrone/IISPH/PressureMirroring/IISPH_PressureMirroring_Marrone_fluid_1_7050.vtu",
